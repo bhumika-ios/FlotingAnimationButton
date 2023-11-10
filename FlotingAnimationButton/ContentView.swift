5de57c338e220f91075d1aa14652638f42151013
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-11-10 12:20:25 +0530

    Add button and set of effect of rotation then set  of background color and shape

--- FlotingAnimationButton/ContentView.swift
+++ FlotingAnimationButton/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var open = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Button(action: { self.open.toggle()}){
+                Image(systemName: "plus")
+                    .rotationEffect(.degrees(open ? 45 : 0))
+                    .foregroundStyle(.white)
+                    .font(.system(size: 38, weight: .bold))
+                    .animation(.spring)
+            }
+            .padding()
+            .background(Color.pink)
+            .mask(Circle())
+            .shadow(color: .pink, radius: 10)
+            .zIndex(10)
         }
-        .padding()
     }
 }
 
