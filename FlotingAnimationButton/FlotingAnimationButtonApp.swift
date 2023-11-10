744e90f847e8bd8e0733513ed83318ca122a9338
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-11-10 12:13:33 +0530

    Initial Commit

---
+++ FlotingAnimationButton/FlotingAnimationButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlotingAnimationButtonApp.swift
+//  FlotingAnimationButton
+//
+//  Created by Bhumika Patel on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FlotingAnimationButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
